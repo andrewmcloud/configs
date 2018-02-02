680517d8fd835e1f5cee303afeeb78f7a1274e2e
Andrew McLoud <amcloud@gmail.com>
2018-02-02 11:23:33 -0500

    lein profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,9 @@
+{:user
+  {
+     :dependencies  [[pjstadig/humane-test-output  "0.8.0"]]
+     :plugins       [[venantius/ultra "0.4.0"]
+                     [lein-kibit "0.1.3"]
+                     [venantius/ultra "0.5.1"]
+                     [lein-cljfmt "0.5.6"]]
+  }
+}
